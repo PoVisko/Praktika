8347fb42551f570269502ea99fb8becdc4df6088
PoVisko <povisko@gmail.com>
2019-01-23 22:43:11 +0200

    Separated main function into smaller functions

--- src/Main.java
+++ src/Main.java
@@ -3,24 +3,12 @@ import java.util.*;
 public class Main {
 
     public static void main(String[] args) {
-        int lowestFloor = -3;
+        int lowestFloor = 7;
         int highestFloor = 8;
-        ArrayList <Integer> totalNumOfFloors = new ArrayList<>();
-        int entryFloor = 0;
-        String vehicleType = "Z";
+        ArrayList <Integer> totalNumOfFloors;
+        int entryFloor = 8;
+        String vehicleType = "V";
         String output;
-
-        /*
-        Fill the list with available floors
-         */
-        for (int i=lowestFloor; i<highestFloor+1; i++){
-            if (i == 0) {
-                continue;
-            }
-            else {
-                totalNumOfFloors.add(i);
-            }
-        }
 
         /*
         Checks the closest parking space depending on vehicle type
@@ -51,6 +39,62 @@ public class Main {
             output = "You can't enter the lot because the floor you're entering to does not exist.";
         }
 
+        /*
+        Prints the output message
+         */
         System.out.println(output);
     }
+
+    /*
+    Validates if entered vehicle type is correct
+     */
+    private static String checkVehicleType (String vehicleType) {
+        if (vehicleType == "P" || vehicleType == "p" || vehicleType == "D" || vehicleType == "d") {
+            return "P";
+        } else if (vehicleType == "E" || vehicleType == "e") {
+            return "E";
+        } else if (vehicleType == "V" || vehicleType == "v") {
+            return "V";
+        } else {
+            return "Error vehicleType";
+        }
+    }
+
+    private static boolean isLowestFloorValid (int lowestFloor, int highestFloor) {
+        if (lowestFloor <= highestFloor) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    /*
+    Fills the list with available parking lot floors
+    */
+    private static ArrayList <Integer> fillListWithFloorNumbers (int lowestFloor, int highestFloor) {
+        ArrayList <Integer> totalNumOfFloors = new ArrayList<>();
+        if (isLowestFloorValid(lowestFloor, highestFloor)) {
+            for (int i=lowestFloor; i < highestFloor+1; i++){
+                if (i == 0) {
+                    continue;
+                }
+                else {
+                    totalNumOfFloors.add(i);
+                }
+            }
+            return totalNumOfFloors;
+        } else {
+            for (int i = highestFloor; i < lowestFloor + 1; i++) {
+                if (i == 0) {
+                    continue;
+                } else {
+                    totalNumOfFloors.add(i);
+                }
+            }
+            return totalNumOfFloors;
+        }
+
+    }
+
+
 }
