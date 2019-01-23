68632292746067f95a9069f17320a6810165b2a6
PoVisko <povisko@gmail.com>
2019-01-23 20:15:11 +0200

    Instantiated variables and created a list to hold all available entry floors

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,33 @@
+import java.lang.reflect.Array;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.IntStream;
 
 
 public class Main {
 
     public static void main(String[] args) {
-        int numLowestFloor;
-        int numHighestFloor;
+        int numLowestFloor = -4;
+        int numHighestFloor = 4;
+        ArrayList <Integer> numTotalFloors = new ArrayList<Integer>();
         int numBoomBarrierFloor;
         char vehicleType;
 
         // Data Testing
         // System.out.println(Arrays.toString(totalFloors.getFloorArray()));
+
+
+        for (int i=numLowestFloor; i<numHighestFloor+1; i++){
+            if (i == 0) {
+                continue;
+            }
+            else {
+                numTotalFloors.add(i);
+            }
+        }
+        System.out.println(numTotalFloors.toString());
+
     }
 }
