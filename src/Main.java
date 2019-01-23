b9a88e23e232561276da4cd5ce258b9a028df4cb
PoVisko <povisko@gmail.com>
2019-01-23 15:44:56 +0200

    Created an array holding all the parking lot floor numbers

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,18 @@
+import Controller.TotalFloors;
+
+
 public class Main {
 
     public static void main(String[] args) {
+        TotalFloors totalFloors = new TotalFloors(-2, 8);
 
+        // Create an array containing all the parking lot floor numbers
+        int floorArrayMin = totalFloors.getLowerFloor();
+        int floorArrayMax = totalFloors.getUpperFloor();
+        int floorArrayCount = floorArrayMax - floorArrayMin + 1;
+        int[] floorArray = new int [floorArrayCount];
+        for (int i = 0; i< floorArrayCount; i++, floorArrayMin++) {
+            floorArray[i] = floorArrayMin;
+        }
     }
 }
