f97a4ebac15106278e990edcb4c09b00d7e9d3d0
PoVisko <povisko@gmail.com>
2019-01-23 18:06:57 +0200

    Get rid of TotalFloors class since we should use functional programming principles

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,13 @@
-import Controller.TotalFloors;
-
 import java.util.Arrays;
 
 
 public class Main {
 
     public static void main(String[] args) {
-        TotalFloors totalFloors = new TotalFloors(-2, 8);
+        int numLowestFloor;
+        int numHighestFloor;
+        int numBoomBarrierFloor;
+        char vehicleType;
 
         // Data Testing
         // System.out.println(Arrays.toString(totalFloors.getFloorArray()));
